d73865889a52b6418fce4444ea0d14e8cb35c890
Cleiton <cleitonncruz105@gmail.com>
2023-10-01 16:02:07 -0300

    Pagamento

--- src/pagseguro/dto/create-pagseguro.dto.ts
+++ src/pagseguro/dto/create-pagseguro.dto.ts
@@ -1,9 +1,6 @@
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreatePagseguroDto {
-  @IsNotEmpty()
-  @IsString()
-  token: string;
 
   @IsNotEmpty()
   @IsNumber()
