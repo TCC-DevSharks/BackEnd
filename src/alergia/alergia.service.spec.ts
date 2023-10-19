bc2150f1dabd2ef31d1f341c9c97c5853978cb41
Cleiton <cleitonncruz105@gmail.com>
2023-10-19 08:34:33 -0300

    test

--- src/alergia/alergia.service.spec.ts
+++ src/alergia/alergia.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AlergiaService } from './alergia.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
 
 describe('AlergiaService', () => {
   let service: AlergiaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AlergiaService],
+      imports:[PrismaModule],
+      providers: [AlergiaService,PrismaService],
     }).compile();
 
     service = module.get<AlergiaService>(AlergiaService);
