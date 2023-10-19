b87a99920dda9f0c0ba9096b377d299dcf40df62
Cleiton <cleitonncruz105@gmail.com>
2023-10-19 10:35:48 -0300

    Update profissional.service.ts

--- src/profissional/profissional.service.ts
+++ src/profissional/profissional.service.ts
@@ -270,7 +270,7 @@ export class ProfissionalService {
       return 'Id Invalid';
     }
     
-    const sql = `select tbl_consulta.id as idConsulta , tbl_especialidade.nome as especialidade, tbl_gestante.nome
+    const sql = `select tbl_consulta.id as idConsulta , tbl_especialidade.nome as especialidade, tbl_gestante.nome,
     date_format(tbl_consulta.dia, '%d/%m/%Y') as dia, time_format(tbl_consulta.hora,'%H:%i:0%s') as hora
     from tbl_gestante
       inner join tbl_consulta
