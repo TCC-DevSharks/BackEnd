6dc6d936ccada4ea33ccaeb538fd001b0ab2b422
Cleiton <cleitonncruz105@gmail.com>
2023-11-19 11:55:28 -0300

    .env

--- src/forum/topic/topic.service.spec.ts
+++ src/forum/topic/topic.service.spec.ts
@@ -2,14 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { DatabaseModule } from '../../mongodb/database.module';
 import { topicProviders } from './topic.providers';
 import { TopicService } from './topic.service';
+import { UserModule } from '../user/user.module';
+import { useProviders } from '../user/user.providers';
+import { UserService } from '../user/user.service';
 
 describe('TopicService', () => {
   let service: TopicService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [DatabaseModule],
-      providers: [TopicService, ...topicProviders],
+      imports: [DatabaseModule, UserModule],
+      providers: [TopicService, ...topicProviders, ...useProviders, UserService],
     }).compile();
 
     service = module.get<TopicService>(TopicService);
